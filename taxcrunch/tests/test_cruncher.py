aaa9cedc56aff6aa8e3712dcae73db3e0e852483
Peter-Metz <peter.metz@aei.org>
2019-05-29 13:02:44 -0400

    add tests

--- taxcrunch/tests/test_cruncher.py
+++ taxcrunch/tests/test_cruncher.py
@@ -10,9 +10,26 @@ CURR_PATH = os.path.abspath(os.path.dirname(__file__))
 
 
 def create_data():
-    print(cr.__file__)
-    c = cr.Cruncher("tests/test_adjustment.json")
+    c = cr.Cruncher(file="tests/test_adjustment.json")
     return c
+
+
+def test_baseline_choice():
+    c = cr.Cruncher(baseline="test_reform.json")
+    assert isinstance(c, cr.Cruncher)
+    with pytest.raises(AttributeError):
+        cr.Cruncher(baseline="fake_file.json")
+
+
+def test_reform_choice():
+    c = cr.Cruncher(
+        file="tests/test_adjustment_noreform.json", custom_reform="test_reform.json"
+    )
+    assert isinstance(c, cr.Cruncher)
+    with pytest.raises(AttributeError):
+        cr.Cruncher(file="tests/test_adjustment.json", custom_reform="test_reform.json")
+    with pytest.raises(AttributeError):
+        cr.Cruncher(custom_reform="fake_file.json")
 
 
 def test_basic_table():
@@ -24,6 +41,7 @@ def test_basic_table():
 def test_calc_table():
     c = create_data()
     table = c.calc_table()
+    assert isinstance(table, pd.DataFrame)
     assert table.iloc[0]["Reform"] + 1 == table.iloc[0]["+ $1"]
     # table.to_csv("expected_calc_table.csv")
     expected_table = pd.read_csv(
@@ -37,4 +55,5 @@ def test_calc_table():
 def test_mtr_table():
     c = create_data()
     table = c.mtr_table()
+    assert isinstance(table, pd.DataFrame)
     assert abs(table.all(axis=None)) < 1
