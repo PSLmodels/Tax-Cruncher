4e7060dc122cd6648cb54751bfaa412820f20c79
Peter-Metz <peter.metz@aei.org>
2019-06-03 16:07:59 -0400

    add compconfig

---
+++ compconfig/compconfig/constants.py
@@ -0,0 +1,43 @@
+import paramtools
+from marshmallow import fields, Schema
+
+POLICY_SCHEMA = {
+    "labels": {
+        "year": {
+            "type": "int",
+            "validators": {
+                "choice": {
+                    "choices": [
+                        yr for yr in range(2013,2028)
+                    ]
+                }
+            }
+        },
+        "MARS": {
+            "type": "str",
+            "validators": {"choice": {"choices": ["single", "mjoint",
+                                                  "mseparate", "headhh",
+                                                  "widow"]}}
+        },
+        "idedtype": {
+            "type": "str",
+            "validators": {"choice": {"choices": ["med", "sltx", "retx", "cas",
+                                                  "misc", "int", "char"]}}
+        },
+        "EIC": {
+            "type": "str",
+            "validators": {"choice": {"choices": ["0kids", "1kid",
+                                                  "2kids", "3+kids"]}}
+        },
+        "data_source": {
+            "type": "str",
+            "validators": {"choice": {"choices": ["PUF", "CPS", "other"]}}
+        }
+    },
+    "additional_members": {
+        "section_1": {"type": "str"},
+        "section_2": {"type": "str"},
+        "start_year": {"type": "int"},
+        "checkbox": {"type": "bool"}
+    }
+}
