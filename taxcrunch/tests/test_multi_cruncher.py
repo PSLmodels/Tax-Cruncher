25c1552c647b710e8888c517cd2009ebb83a3c15
Reid McLaughlin <reidmcl@gmail.com>
2019-07-15 14:05:30 -0400

    table test

--- taxcrunch/tests/test_multi_cruncher.py
+++ taxcrunch/tests/test_multi_cruncher.py
@@ -37,3 +37,16 @@ def test_get_pol_link():
     reform_preset = "Trump2016.json"
     m = b.get_pol(reform_file=reform_preset)
     assert m._II_rt1[2017-2013] == 0.12
+
+CURR_PATH = os.path.abspath(os.path.dirname(__file__))
+
+def test_calc_table():
+    b = mcr.Batch(path="example_test_input.csv")
+    table = b.create_table()
+    assert isinstance(table, pd.DataFrame)
+    table.to_csv("expected_calc_table.csv")
+    # expected_table = pd.read_csv(
+        os.path.join(CURR_PATH, "expected_calc_table.csv"), index_col=0
+    )
+    for col in table.columns:
+        assert np.allclose(table[col], expected_table[col])
