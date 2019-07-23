79c1b3cbddcd90920365aeeb74002aa16700606c
Peter-Metz <peter.metz@aei.org>
2019-07-23 16:18:01 -0400

    update test file paths

--- taxcrunch/tests/test_multi_cruncher.py
+++ taxcrunch/tests/test_multi_cruncher.py
@@ -8,13 +8,13 @@ import taxcrunch.cruncher as cr
 import taxcrunch.multi_cruncher as mcr
 
 def test_get_pol_no_reform():
-    b = mcr.Batch(path="example_test_input.csv")
+    b = mcr.Batch(path="tests/example_test_input.csv")
     assert isinstance(b, mcr.Batch)
     m = b.get_pol(reform_file=None)
     assert isinstance(m,tc.Policy)
 
 def test_get_pol_directory_file():
-    b = mcr.Batch(path="example_test_input.csv")
+    b = mcr.Batch(path="tests/example_test_input.csv")
     # use the full file pathname for testing purposes
     local_reform = "tests/test_reform.json"
     n = b.get_pol(reform_file=local_reform) 
@@ -25,12 +25,12 @@ reform_dict = {
     }
 
 def test_get_pol_dict():
-    b = mcr.Batch(path="example_test_input.csv")
+    b = mcr.Batch(path="tests/example_test_input.csv")
     m = b.get_pol(reform_file=reform_dict)
     assert m._CTC_c[2018-2013] == 1800
 
 def test_get_pol_link():
-    b = mcr.Batch(path="example_test_input.csv")
+    b = mcr.Batch(path="tests/example_test_input.csv")
     reform_preset = "Trump2016.json"
     m = b.get_pol(reform_file=reform_preset)
     assert m._II_rt1[2017-2013] == 0.12
@@ -38,7 +38,7 @@ def test_get_pol_link():
 CURR_PATH = os.path.abspath(os.path.dirname(__file__))
 
 def test_calc_table():
-    b = mcr.Batch(path="example_test_input.csv")
+    b = mcr.Batch(path="tests/example_test_input.csv")
     table = b.create_table()
     assert isinstance(table, pd.DataFrame)
     # table.to_csv("expected_multi_table.csv")
