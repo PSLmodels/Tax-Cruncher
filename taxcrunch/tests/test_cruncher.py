c4bc15a090d95746bba70155b779a8163c9cff99
Peter-Metz <peter.metz@aei.org>
2020-08-25 12:40:00 -0400

    commented wrong line

--- taxcrunch/tests/test_cruncher.py
+++ taxcrunch/tests/test_cruncher.py
@@ -44,8 +44,8 @@ def test_calc_table():
     table = c.calc_table()
     assert isinstance(table, pd.DataFrame)
     assert table.iloc[0]["Reform"] + 1 == table.iloc[0]["+ $1 (Taxpayer Earnings)"]
-    # table_path = os.path.join(CURR_PATH, "expected_calc_table.csv")
-    table.to_csv(table_path)
+    table_path = os.path.join(CURR_PATH, "expected_calc_table.csv")
+    # table.to_csv(table_path)
     expected_table = pd.read_csv(table_path, index_col=0)
     diffs = False
     for col in table.columns:
