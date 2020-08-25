0810bc3ffab3e052c7ccaafc7c3589814b9a7229
Peter-Metz <peter.metz@aei.org>
2020-08-25 12:27:05 -0400

    remove helpers.py translation from web app

--- cs-config/cs_config/tests/test_functions.py
+++ cs-config/cs_config/tests/test_functions.py
@@ -1,6 +1,6 @@
 from cs_kit import CoreTestFunctions
 
-from cs_config import functions, helpers
+from cs_config import functions
 
 
 class TestFunctions1(CoreTestFunctions):
@@ -11,30 +11,3 @@ class TestFunctions1(CoreTestFunctions):
     ok_adjustment = {"Tax Information": {"mstat": [{"value": "Joint"}]}, "Policy":{}}
     get_inputs = functions.get_inputs
     get_version = functions.get_version
-
-
-def test_convert_adj():
-    adj = {
-        "STD": [
-            {"MARS": "single", "year": "2019", "value": 0},
-            {"MARS": "mjoint", "year": 2019, "value": 1}
-        ],
-        "EITC_c": [{"EIC": "0kids", "year": "2019", "value": 1000.0}],
-        "BEN_ssi_repeal": [
-            {"year": 2019, "value": True}
-        ]
-    }
-
-    res = helpers.convert_adj(adj, 2019)
-
-    assert res == {
-        "STD": {
-            2019: [0, 1, 12200.0, 18350.0, 24400.0]
-        },
-        "EITC_c": {
-            2019: [1000.0, 3526.0, 5828.0, 6557.0]
-        },
-        "BEN_ssi_repeal": {
-            2019: True
-        }
-    }
