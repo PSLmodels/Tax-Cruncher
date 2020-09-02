c121fd35cc2613d5dab9130109900914a7ab868b
Hank Doupe <henrymdoupe@gmail.com>
2020-09-02 11:41:32 -0400

    Use ParamTools>0.15.1

--- cs-config/install.sh
+++ cs-config/install.sh
@@ -2,5 +2,5 @@
 
 git clone https://github.com/PSLmodels/Tax-Cruncher
 cd Tax-Cruncher
-conda install PSLmodels::taxcalc PSLmodels::behresp conda-forge::paramtools "bokeh<2.0.0" ipython
+conda install PSLmodels::taxcalc PSLmodels::behresp "conda-forge::paramtools>=0.15.1" "bokeh<2.0.0" ipython
 pip install -e .
