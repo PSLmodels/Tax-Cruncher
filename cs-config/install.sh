baff46a7b0bdb0e514e99920b0e7ab55123f5568
Kcreek Dev <kcreekdev@gmail.com>
2024-02-28 19:22:20 -0500

    Remove cs-config/install.sh script to use environment.yml

--- cs-config/install.sh
+++
@@ -1,6 +0,0 @@
-# bash commands for installing your package
-
-git clone https://github.com/PSLmodels/Tax-Cruncher
-cd Tax-Cruncher
-conda install PSLmodels::taxcalc PSLmodels::behresp "conda-forge::paramtools>=0.15.1" "bokeh<2.0.0" ipython
-pip install -e .
